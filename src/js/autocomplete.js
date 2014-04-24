e98838a5c137319a2adbfc081b992d2267b6dcb2
Bartek <barfek@gmail.com>
2014-04-24 11:25:54 +0200

    turned native browes autoc. off; searchTerm.length shouldn't count whitespace

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -59,6 +59,7 @@ define([ "jquery" ], function($) {
     // I like this method of storing methods and then attaching to the prototype at the end...
 
     init: function() {
+      this.$el.attr("autocomplete", "off"); // turn off native browser autocomplete feature
       this.wrapEl();
       this.setupListeners();
     },
@@ -182,7 +183,7 @@ define([ "jquery" ], function($) {
     processSearch: function(searchTerm) {
       var _this = this;
       this.resultIndex = 0;
-      if (searchTerm && searchTerm.length >= this.config.threshold) {
+      if (searchTerm && searchTerm.trim().length >= this.config.threshold) {
         this.callFetch(searchTerm, function() {
           _this.populateResultPanel();
         });
