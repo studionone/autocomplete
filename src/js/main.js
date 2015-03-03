3050d5bef751eb74adc0e638c7295448383ff8ba
Bartek <barfek@gmail.com>
2015-03-04 00:08:36 +0100

    Refactor & add functionality
    New features:
    - forceSelection option: basically, if set to `true`, autocomplete won't let user choose anything that is not provided on the search results panel; either something gets chosen or input gets cleared.
    - Disabled item support: items with `disabled: true` property will be unclickable & have an extra class added.
    - Loading data indication: `is-loading` will be added to `$wrapper` each time fetching starts & removed when it's done
    - Empty item template: if defined, it will be displayed if `fetch` function returns empty results array.
    - Support for tab key: selects currently highlighted item.
    
    Tweaks:
    - Special keys will react on `keydown`, not `keyup` (no more weird delay).
    
    Backwards-incompatible changes:
    - Removed support for custom html node templates. Base classes are now fixed, but extra classes can still be added via `extraClasses` config object.

--- src/js/main.js
+++ src/js/main.js
@@ -1,61 +1,65 @@
 require.config({
-  "paths": {
-    "jquery": "/bower_components/jquery/dist/jquery",
-    "data": "/src/js/data",
-    "autocomplete": "/src/js/autocomplete"
+  paths: {
+    jquery: "/bower_components/jquery/dist/jquery",
+    data: "/src/js/data",
+    autocomplete: "/src/js/autocomplete"
   },
   shim: {
-    "jquery": {
+    jquery: {
       exports: "$"
     }
   }
 });
 
 require([ "data", "jquery", "autocomplete" ], function(data, $, AutoComplete) {
-  
+
   "use strict";
 
-  var customFetch = function(searchTerm, cb) {
+  var customFetch, customOnItem;
+
+  customFetch = function(searchTerm, cb) {
     var results = [],
-        searchFields = ["Country", "City", "Company"],
-        matchFlags = [];
+        searchFields = [ "Country", "City", "Company" ],
+        matchFlag = false;
     searchTerm = searchTerm.toLowerCase().trim().split(" "); // for space-divided multi-term search
     for (var i = 0; i < data.length; i++) {
       // Init/reset matchFlags for current searchTerms
-      matchFlags = [false];
+      matchFlag = false;
       for (var j = 0; j < searchTerm.length; j++) {
         for (var k = 0; k < searchFields.length; k++) {
           // If (in current data obj) any searchField matches current searchTerm - set current matchFlag true.
-          matchFlags[j] =
-            (data[i][searchFields[k]].toLowerCase().indexOf(searchTerm[j]) != -1) || matchFlags[j];
+          matchFlag = matchFlag || data[i][searchFields[k]].toLowerCase().indexOf(searchTerm[j]) != -1;
         }
       }
       // If (for current searchFields) all searchTerms flags are true, push current data obj to results.
-      if (matchFlags.reduce(function(prev, curr, i, arr){ return  prev && curr; })) {
-        results.push(data[i]);
-      }
+      matchFlag && results.push(data[i]);
     }
     cb(results);
   };
 
-  var customOnItem = function(el) {
-    var company = $(el).attr("data-company");
-    $("#autocomplete1").val(company);
+  customOnItem = function(el) {
+    if (!el.dataset.disabled) {
+      var company = $(el).attr("data-company");
+      $("#autocomplete1").val(company);
+    }
   };
 
-  var x = new AutoComplete({
+  new AutoComplete({
     el: "#autocomplete1",
     threshold: 2,
     limit: 5,
+    forceSelection: true, // input value has to come from results
     fetch: customFetch,
     template: {
       // Custom html tags are supported.
       // Multiple classes per element are supported, but the first one will always be an element reference.
-      // Every element needs to have at least 1 unique class defined for plugin to work.
+      // Every element needs to have at least 1 unique class for plugin to work.
       elementWrapper: "<div class='js-autocomplete'></div>",
       resultsWrapper: "<div class='autocomplete'></div>",
       resultsContainer: "<ul class='autocomplete__results'></ul>",
       resultsItem: "<li class='autocomplete__results__item' data-company='{{Company}}'><strong>{{Company}}</strong><br/><small>{{City}}, {{Country}}</small></li>",
+      // noResultsItem can be undefined. It simply will not be displayed then.
+      noResultsItem: "<li class='autocomplete__results__item' data-disabled='true'>No matches found</li>",
       resultsItemHighlightClass: "autocomplete__results__item--highlight",
       searchTermHighlightClass: "autocomplete__search-term--highlight",
       hiddenClass: "is-hidden"
