b74fd7e3ad4efc1942f106fe13db73d6476ad87e
Bartek <barfek@gmail.com>
2015-04-02 19:49:32 +0100

    Update example

--- src/js/example.js
+++ src/js/example.js
@@ -6,46 +6,40 @@ require.config({
   }
 });
 
-require([ "jquery", "data", "autocomplete" ], function($, data, AutoComplete) {
+require([ "jquery", "data", "autocomplete" ], function($, data, Autocomplete) {
 
   "use strict";
 
-  var $input, customFetch;
-
   $(document).ready(function() {
 
-    $input = $(".js-autocomplete-company");
-
-    customFetch = function(searchTerm, cb) {
-      var results = [],
-          searchFields = [ "Country", "City", "Company" ],
-          foundMatch, i, j, k;
-
-      searchTerm = $.trim(searchTerm.toLowerCase()).split(" ");
-
-      for (i = 0; i < data.length; i++) {
-        foundMatch = false;
-        for (j = 0; j < searchTerm.length; j++) {
-          for (k = 0; k < searchFields.length; k++) {
-            foundMatch = foundMatch || data[i][searchFields[k]].toLowerCase().indexOf(searchTerm[j]) != -1;
-          }
-        }
-        foundMatch && results.push(data[i]);
-      }
-
-      setTimeout(function() { cb(results); }, 500); // setTimeout to simulate data loading delay
-    };
-
-    $input.length && new AutoComplete({
-      el: $input,
+    new Autocomplete({
+      el: ".js-autocomplete-company",
       threshold: 2,
       limit: 5,
       forceSelection: true,
-      fetch: customFetch,
       templates: {
         item: "{{Company}}<br /><small>{{City}}, {{Country}}</small>",
         value: "{{Company}}",
       },
+      fetch: function(searchTerm, cb) {
+        var results = [],
+            searchFields = [ "Country", "City", "Company" ],
+            foundMatch, i, j, k;
+
+        searchTerm = $.trim(searchTerm.toLowerCase()).split(" ");
+
+        for (i = 0; i < data.length; i++) {
+          foundMatch = false;
+          for (j = 0; j < searchTerm.length; j++) {
+            for (k = 0; k < searchFields.length; k++) {
+              foundMatch = foundMatch || data[i][searchFields[k]].toLowerCase().indexOf(searchTerm[j]) != -1;
+            }
+          }
+          foundMatch && results.push(data[i]);
+        }
+
+        setTimeout(function() { cb(results); }, 500); // setTimeout to simulate data loading delay
+      }
     });
 
   });
